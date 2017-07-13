fe1e025dfd898b4a5401c601ddb3b8a3be9e5b5f
DaceT <shondace_thomas@yahoo.com>
2017-07-13 14:56:37 -0400

    added a heading

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -5,10 +5,10 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-    	logging.info("MainHandler")
+        logging.info("MainHandler")
         html_params = {
             "title": "Main Title",
-            "content": "Hello"
+            "content": ""
         }
         template = jinja_env.env.get_template('templates/tmpl.html')
         self.response.out.write(template.render(html_params))
