219bb0b5e41953c7aa6200fb754fcb714cabf347
malaika88 <malibyll2017@gmail.com>
2017-07-17 14:13:52 -0400

    form and data model

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -12,3 +12,12 @@ class MainHandler(webapp2.RequestHandler):
         }
         template = jinja_env.env.get_template('templates/tmpl.html')
         self.response.out.write(template.render(html_params))
+    def post(self):
+    	logging.info("There was a post.")
+    	r_breakfast=self.request.get("form_breakfast")
+    	r_lunch=self.request.get("form_lunch")
+    	r_dinner=self.request.get("form_dinner")
+    	r_brkfst_calories=self.request.get("brkfst_calories")
+    	r_lunch_calories=self.request.get("lunch_calories")
+    	r_dinner_calories=self.request.get("dinner_calories")
+
