d5567bd257e17af1704ca31d24885d89126b96cc
Khagriffey <marleynembhard1227@gmail.com>
2017-07-19 16:01:39 -0400

    fix everything

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import webapp2
 from handlers import jinja_env
 from handlers import main_handler
 from handlers import about_handler
-from handlers import graph_handler
-from handlers import history_handler
 from handlers import recommend_handler
 
 
@@ -15,8 +13,6 @@ jinja_env.init(os.path.dirname(__file__))
 app = webapp2.WSGIApplication([
     ('/', main_handler.MainHandler),
     ('/about', about_handler.AboutHandler),
-    ('/graph', graph_handler.GraphHandler),
-    ('/history', history_handler.HistoryHandler),
     ('/recommend', recommend_handler.RecommendHandler),
 ], debug=True)
 
