1be6a1e6a68aeb49f3cb8e14320127532e683f46
DaceT <shondace_thomas@yahoo.com>
2017-07-20 13:37:51 -0400

    changes

--- handlers/recommend_handler.py
+++ handlers/recommend_handler.py
@@ -12,21 +12,21 @@ class RecommendHandler(webapp2.RequestHandler):
 		r_b_cal=self.request.get("brkfst_calories")
 		r_l_cal=self.request.get("lunch_calories")
 		r_d_cal=self.request.get("dinner_calories")
-		totalcal =float(r_b_cal) +float(r_l_cal) +float(r_d_cal)
-		logging.info(totalcal)
+		totalCal =float(r_b_cal) + float(r_l_cal) + (float)r_d_cal
+		logging.info(totalCal)
 		logging.info("RecommendHandler")
 		html_params = {
 			"title": "Fresh Fit",
 			"content": "",
-			"totalCal":totalcal
+			"totalCal":totalCal
 			}
 		template = jinja_env.env.get_template('templates/recommend.html')
 
-		if totalcal <= 2600:
+		if totalCal <= 2600:
 			html_params ["imageurl"]= "http://hipnewjersey.com/wp-content/uploads/2017/06/istock-499609170-1.jpg"
-		elif totalcal < 3000:
+		elif totalCal < 3000:
 			html_params ["imageurl"]= "https://squashskills.com/images/sized/1400x660/1063618794-poi824.290-qx100.png"
-		elif totalcal >= 3000:
+		elif totalCal >= 3000:
 			html_params ["imageurl"] = "http://www.blakedating.com/wp-content/uploads/2016/11/Gym-Course.jpg"
 		
 		self.response.out.write(template.render(html_params))
